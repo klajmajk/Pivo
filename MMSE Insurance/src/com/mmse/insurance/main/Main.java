778a669390498b919f1979789ec3a94a287f0512
Lucas Sartore <lucas.sartore@gmail.com>
2013-10-24 09:15:46 +0200

    Adding a test

--- MMSE Insurance/src/com/mmse/insurance/main/Main.java
+++ MMSE Insurance/src/com/mmse/insurance/main/Main.java
@@ -5,5 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
 
 }
---
+++ MMSE Insurance/src/com/mmse/insurance/main/testMain.java
@@ -0,0 +1,14 @@
+package com.mmse.insurance.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testMain {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
